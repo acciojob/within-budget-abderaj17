8250a8f04411bbd454aa21713af0ebd676beb89a
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-01-11 20:55:36 +0530

    fom

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [budget, setBudget] = useState(0);
+
+  const items = [
+    { name: "Apple", price: 20 },
+    { name: "Banana", price: 10 },
+    { name: "Nutella", price: 30 },
+    { name: "Razor", price: 5 },
+    { name: "CornFlakes", price: 15 },
+    { name: "Sound Bar", price: 50 },
+    { name: "iPhone", price: 80 },
+  ];
+
+
+  const handleInput = (e)=>{
+   let value = parseInt(e.target.value, 10);
+    setBudget(value || 0);
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+     <h2>Enter your budget to check available items:</h2>
+     <input type="number" placeholder="Enter your budget" onChange={handleInput} />
+
+     <h4>Items you can buy are in Green color</h4>
+     <br/>
+     <table className="table">
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index)=>(
+          <tr key={index}
+          style={{
+            color: item.price<= budget ? "green" : "red",
+          }}>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+          </tr>
+        ))}
+      </tbody>
+     </table>
     </div>
   )
 }
